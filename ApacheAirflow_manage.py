75080e94c98b114636e752c2f4ca1f8f543a2dad
Hikari <118663358+HikariJadeEmpire@users.noreply.github.com>
2023-07-20 14:34:33 +0700

    update 20.07.2023

--- ApacheAirflow_manage.py
+++ ApacheAirflow_manage.py
@@ -5,30 +5,28 @@ from airflow.utils.dates import days_ago
 import pandas as pd
 import requests
 
-MYSQL_CONNECTION = "mysql_default"   # ชื่อของ connection ใน Airflow ที่เซ็ตเอาไว้
+MYSQL_CONNECTION = "mysql_default"   # name of connection in Airflow
 CONVERSION_RATE_URL = "https://r2de2-workshop-vmftiryt6q-ts.a.run.app/usd_thb_conversion_rate"
 
-# path ที่จะใช้
+# mounted path
 mysql_output_path = "/home/airflow/gcs/data/audible_data_merged.csv"
 conversion_rate_output_path = "/home/airflow/gcs/data/conversion_rate.csv"
 final_output_path = "/home/airflow/gcs/data/output.csv"
 
 
 def get_data_from_mysql(transaction_path):
-    # รับ transaction_path มาจาก task ที่เรียกใช้
 
-    # เรียกใช้ MySqlHook เพื่อต่อไปยัง MySQL จาก connection ที่สร้างไว้ใน Airflow
+    # call MySqlHook to connect to MySQL
     mysqlserver = MySqlHook(MYSQL_CONNECTION)
     
-    # Query จาก database โดยใช้ Hook ที่สร้าง ผลลัพธ์ได้ pandas DataFrame
     audible_data = mysqlserver.get_pandas_df(sql="SELECT * FROM audible_data")
     audible_transaction = mysqlserver.get_pandas_df(sql="SELECT * FROM audible_transaction")
 
-    # Merge data จาก 2 DataFrame เหมือนใน workshop1
+    # Merge data ( like S01_DATACollection.ipynb )
     df = audible_transaction.merge(audible_data, how="left", left_on="book_id", right_on="Book_ID")
 
-    # Save ไฟล์ CSV ไปที่ transaction_path ("/home/airflow/gcs/data/audible_data_merged.csv")
-    # จะไปอยู่ที่ GCS โดยอัตโนมัติ
+    # Save CSV file to transaction_path ("/home/airflow/gcs/data/audible_data_merged.csv")
+    
     df.to_csv(transaction_path, index=False)
     print(f"Output to {transaction_path}")
 
@@ -38,14 +36,13 @@ def get_conversion_rate(conversion_rate_path):
     result_conversion_rate = r.json()
     df = pd.DataFrame(result_conversion_rate)
 
-    # เปลี่ยนจาก index ที่เป็น date ให้เป็น column ชื่อ date แทน แล้วเซฟไฟล์ CSV
     df = df.reset_index().rename(columns={"index": "date"})
     df.to_csv(conversion_rate_path, index=False)
     print(f"Output to {conversion_rate_path}")
 
 
 def merge_data(transaction_path, conversion_rate_path, output_path):
-    # อ่านจากไฟล์ สังเกตว่าใช้ path จากที่รับ parameter มา
+    
     transaction = pd.read_csv(transaction_path)
     conversion_rate = pd.read_csv(conversion_rate_path)
 
@@ -56,14 +53,13 @@ def merge_data(transaction_path, conversion_rate_path, output_path):
     # merge 2 DataFrame
     final_df = transaction.merge(conversion_rate, how="left", left_on="date", right_on="date")
     
-    # แปลงราคา โดยเอาเครื่องหมาย $ ออก และแปลงให้เป็น float
     final_df["Price"] = final_df.apply(lambda x: x["Price"].replace("$",""), axis=1)
     final_df["Price"] = final_df["Price"].astype(float)
 
     final_df["THBPrice"] = final_df["Price"] * final_df["conversion_rate"]
     final_df = final_df.drop(["date", "book_id"], axis=1)
 
-    # save ไฟล์ CSV
+    # save to CSV
     final_df.to_csv(output_path, index=False)
     print(f"Output to {output_path}")
     print("== End of Workshop 4 ʕ•́ᴥ•̀ʔっ♡ ==")
@@ -76,8 +72,8 @@ with DAG(
     tags=["workshop"]
 ) as dag:
 
-    # TODO: สร้าง t1, t2, t3 ที่ใช้ PythonOperator 
-    # และสร้าง task dependencies
+    # TODO: create t1, t2, t3 using PythonOperator
+
     t1 = PythonOperator(
         task_id="get_mySQL",
         python_callable = get_data_from_mysql,
@@ -100,4 +96,5 @@ with DAG(
                     },
     )
 
+    # create task dependencies
     [t1,t2] >> t3    
