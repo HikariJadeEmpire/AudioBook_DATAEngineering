74556865d8c3567b6fc7aa4a0fba1e7e360580cd
Hikari <118663358+HikariJadeEmpire@users.noreply.github.com>
2023-07-21 07:51:33 +0700

    Update ApacheAirflow_manage.py

--- ApacheAirflow_manage.py
+++ ApacheAirflow_manage.py
@@ -1,5 +1,6 @@
 from airflow.models import DAG
 from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
 from airflow.providers.mysql.hooks.mysql import MySqlHook
 from airflow.utils.dates import days_ago
 import pandas as pd
@@ -96,5 +97,14 @@ with DAG(
                     },
     )
 
+    t4 = BashOperator(
+        task_id="To_BigQuery",
+        bash_command = 'bq load \
+                --source_format=CSV \
+                --autodetect myworkshop.audible_data \
+                gs://asia-east2-hikari-workshops-4a6d711e-bucket/data/output.csv'
+    )
+
     # create task dependencies
-    [t1,t2] >> t3    
+
+    [t1,t2] >> t3 >> t4
